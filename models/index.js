04594feefd9b1770ad2c58a715488797384d11c7
ct <charleswtiffany@gmail.com>
2024-02-06 17:48:00 -0800

    added code to js files in models folder

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.hasOne(Category, {
+  foreignKey: 'Products_id,',
+  onDelete: 'CASCADE'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.BelongsToMany(Product, {
+  foreignKey: 'product_id'
+})
 
 module.exports = {
   Product,
