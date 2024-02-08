5c6f2b7fc76d69e033efbfae78964794ffdf72cd
ct <charleswtiffany@gmail.com>
2024-02-08 15:07:57 -0800

    added code to tage-routes.js and minor fixes to other files as well as installed nodemon to start

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -125,6 +125,7 @@ router.delete('/:id', async (req, res) => {
       return;
     }
 
+    res.status(200).json({ message: 'Product deleted'})
   } catch (err) {
     res.status(500).json(err)
   }
